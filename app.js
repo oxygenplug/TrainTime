8ad3583447116e9a873304b3e0a004b9761d1e8f
oxygenplug <dan.garces2@gmail.com>
2018-03-12 22:29:19 -0400

    working on calculating next arrival time and minutes away

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
 var config = {
   apiKey: "AIzaSyAiVvzFKaP24b7Jsw1mAxp0Gw75kF7nduI",
-    authDomain: "traintime-47b98.firebaseapp.com",
-    databaseURL: "https://traintime-47b98.firebaseio.com",
-    projectId: "traintime-47b98",
-    storageBucket: "",
-    messagingSenderId: "347695350504"
+  authDomain: "traintime-47b98.firebaseapp.com",
+  databaseURL: "https://traintime-47b98.firebaseio.com",
+  projectId: "traintime-47b98",
+  storageBucket: "",
+  messagingSenderId: "347695350504"
 };
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 // 2. Button for adding Employees
-$("#submitBtn").on("click", function(event) {
+$("#submitBtn").on("click", function (event) {
   event.preventDefault();
 
   // Grabs user input
@@ -41,6 +41,35 @@ $("#submitBtn").on("click", function(event) {
   // Alert
   alert("A New Train Schedule Has Been Added!");
 
+  function calculateArrival() {
+    var aT = $("#arrivalTime").val().trim();
+    var slicedAt = aT.slice(0, 1);
+    var d = new Date;
+    var h = d.getHours();
+    console.log(h);
+    var nextArrival = 0;
+
+    do {
+      slicedAt = slicedAt  + frequency; 
+
+    }
+    while(slicedAt < h );
+
+  
+  }
+
+  calculateArrival();
+
+  $("#trainDisplayData").append("<tr>" +
+                                "<th>" + trainName + "</th>" + 
+                                "<th>" + dest + "</th>" + 
+                                "<th>" + frequency +  "</th>" + 
+                                "<th>" +  + "</th>" + 
+                                "<th>" +  + "</th>"
+                                 +"</tr>")
+
+
+
   // Clears all of the text-boxes
   $("#trainName").val("");
   $("#destination").val("");
@@ -48,8 +77,8 @@ $("#submitBtn").on("click", function(event) {
   $("#frequency").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+database.ref("Trains").on("child_added", function (childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
 
